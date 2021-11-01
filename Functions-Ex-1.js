b71b2f72342238fdff7682909d8396b6b2082193
sudhasew <sudha.sew@gmail.com>
2021-11-01 13:36:13 -0400

    final commit

--- Functions-Ex-1.js
+++ Functions-Ex-1.js
@@ -3,25 +3,25 @@ function getAreaOfCircle(radius) {
   return circleArea;
 }
 let cArea = getAreaOfCircle(6);
-console.log(cArea);
+console.log(`${cArea}`);
 
 function getCircumferenceOfCircle(radius) {
   let circumferance = 2 * Math.PI * radius;
   return circumferance;
 }
 let cCircum = getCircumferenceOfCircle(6);
-console.log(cCircum);
+console.log(`${cCircum}`);
 
 function getAreaOfSquare(side) {
   let squareArea = side * side;
   return squareArea;
 }
 let aSquare = getAreaOfSquare(4);
-console.log(aSquare);
+console.log(`${aSquare}`);
 
 function getAreaOfTriangle(base, height) {
   let triangleArea = (height * base) / 2;
   return triangleArea;
 }
 let aTriangle = getAreaOfTriangle(5, 8);
-console.log(aTriangle);
+console.log(`${aTriangle}`);
